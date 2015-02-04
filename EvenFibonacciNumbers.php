287cf6633cbb573d52a8d24d44045bad6e6378ad
Thomas <info@von-gostomski.eu>
2015-02-04 19:50:44 +0100

    solved problem #2

--- EvenFibonacciNumbers.php
+++ EvenFibonacciNumbers.php
@@ -1,1 +1,17 @@
 <?php
+
+
+$x = 1;
+$y = 2;
+$sum = $y;
+while(true) {
+
+    $z = $x + $y;
+    if($z > 4000000){
+        break;
+    }
+    $x = $y;
+    $y = $z;
+    if($z%2==0) $sum =$sum+$z;
+}
+print_r($sum);
