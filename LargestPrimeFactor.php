e7d0a4b8bb8c7795e9dfb2147280a8c1d02d706f
Thomas <info@von-gostomski.eu>
2015-02-06 21:05:30 +0100

    ...

--- LargestPrimeFactor.php
+++ LargestPrimeFactor.php
@@ -3,16 +3,16 @@
 $number = 600851475143;
 $start = microtime(true);
 for ($i = 2; $i <= sqrt($number); $i++) {
-    if (($number / $i) === ceil($number / $i)) {
+    if (($number / $i) == ceil($number / $i)) {
         if (is_prim($i)) {
-            echo $i . "<br>\n";
+            echo $i . "\n";
         }
     }
 }
 function is_prim($primetest)
 {
     for ($i = $primetest - 1; $i >= 2; $i--) {
-        if ($primetest % $i === 0) {
+        if ($primetest % $i == 0) {
             return false;
         }
     }
