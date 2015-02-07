84034757213a6a9a419a5a50b4c18606015bc360
Thomas <info@von-gostomski.eu>
2015-02-08 00:01:55 +0100

    commit

--- The10001stPrime.php
+++ The10001stPrime.php
@@ -4,17 +4,15 @@ class The10001stPrime extends PrimeStuff{
 
     public function getPositionedPrimeNumber($position)
     {
-        $i = 1;
-        $prim = 3;
-        $result = 0;
-        while ($i <= $position) {
-            if (self::isPrime($prim)) {
-                echo $i ."\n";
-                $i++;
-                $result = $prim;
+        $primeCount = 0;
+        for($i=1; ; $i++) {
+            if(PrimeStuff::isPrime($i)) {
+                $primeCount++;
+                if ($primeCount === $position) {
+                    return $i;
+                }
             }
-            $prim++;
         }
-        return $result;
+        return;
     }
 }
