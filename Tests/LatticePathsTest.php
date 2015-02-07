665f01f2854bb34c8c5b3562751caa4f1b30ec76
Thomas <info@von-gostomski.eu>
2015-02-08 00:26:04 +0100

    solved #15

---
+++ Tests/LatticePathsTest.php
@@ -0,0 +1,24 @@
+<?php
+require "../LatticePaths.php";
+class LatticePathsTest extends PHPUnit_Framework_TestCase{
+
+    /**
+     * @test
+     * @return void
+     */
+    public function checkPossiblePathsForSmallSquare()
+    {
+        $class = new LatticePaths();
+            $this->assertSame(6, $class->getPathsForSquare(2));
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function solutionProof()
+    {
+        $class = new LatticePaths();
+        $this->assertSame(137846528820, $class->getPathsForSquare(20));
+    }
+}
